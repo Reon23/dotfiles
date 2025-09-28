276c447a0804acaaacabf4ce6b929006c77a291f
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-09-28 22:10:28 +0530

    fixed: wallpaper glitch on boot

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # === CONFIG ===
-WALLPAPER="$HOME/Pictures/wallpaper/default.png"
+WALLPAPER="$HOME/Pictures/wallpaper/default.jpg"
 
 # Wait a bit to ensure Hyprland is ready
-
+sleep 1
 # Generate colors with pywal
 wal -i "$WALLPAPER" --backend wal
