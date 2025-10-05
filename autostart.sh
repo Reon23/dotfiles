6bae724b425b8c37862fecdcd43108d160999f9c
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-10-05 19:29:01 +0530

    Modified: default wal on load

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # === CONFIG ===
-WALLPAPER="$HOME/Pictures/wallpaper/default.jpg"
+WALLPAPER="$HOME/Pictures/wallpaper/a_road_with_trees_on_either_side_of_it.jpg"
 
 # Wait a bit to ensure Hyprland is ready
 sleep 1
